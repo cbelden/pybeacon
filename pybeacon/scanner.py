9a79f37ea90560b23fed0b726d10d952221bd314
Calvin Belden <cbelden@nd.edu>
2014-03-27 10:36:22 -0400

    Update scanner.py documentation.

--- pybeacon/scanner.py
+++ pybeacon/scanner.py
@@ -6,14 +6,7 @@ from logger import BeaconLogger
 
 class Scanner():
     def __init__(self, debug=False, loghandler=logging.StreamHandler()):
-        """
-        Overview:
-            Initializes the Scanner object. If logging is true, a logger is created that writes to
-            the specified handler (stderr if not specified).
-        Input:
-            -logging: specifies whether or not logging is turned on
-            -loghandler: particular log handler (specifies output destination)
-        """
+        """Creates a new Scanner object."""
         # Set up beacon logging
         self._beaconlog = BeaconLogger('beacon-logger')
 
@@ -26,31 +19,22 @@ class Scanner():
             self._log.setLevel(logging.INFO)
 
     def _lescan(self):
-        """
-        Overview:
-            Executes the hcitool lescan command and checks for expected output.
-
-        Returns:
-            True if output is standard upon successful execution; returns false if the command fails.
-        """
+        """Executes the hcitool lescan command and checks for expected output."""
         self._log.info('Starting LEscan...')
         command = ['sudo', 'stdbuf', '-oL', 'hcitool', 'lescan']
         lescan = subprocess.Popen(command, stdout=subprocess.PIPE)
         r = lescan.stdout.readline()
 
-        # return true if output is regular; false otherwise
+        # Check if output is indicative of a successful lescan
         if r != 'LE Scan ...\n':
-            # need to 'sudo kill' process because it is executed as root
+            # Kill process and return False
             os.system('sudo kill %s' % lescan.pid)
             return False
 
         return True
 
     def _toggle_dongle(self):
-        """
-        Overview:
-            Toggles the Bluetooth dongle: off/on.
-        """
+        """Toggles the Bluetooth dongle: off/on."""
         # Turn Bluetooth dongle off
         self._log.info('Turning off bt dongle')
         command = ['sudo', 'hciconfig', 'hci0', 'down']
@@ -64,10 +48,7 @@ class Scanner():
         p.wait()
 
     def _recover_lescan(self):
-        """
-        Overview:
-            Attempts to restart the lescan command. Exits if this fails.
-        """
+        """Attempts to restart the lescan command. Exits if this fails."""
         self._log.warn("LEscan command failed. Attempting to recover.")
         
         # Toggling on and off the bt dongle historically works
@@ -79,26 +60,18 @@ class Scanner():
             exit(1)
 
     def _hcidump(self):
-        """
-        Overview:
-            Executes the hcitools hcidump command which outputs all bluetooth activity.
-        Returns:
-            -hcidump process handle
-        """
+        """Executes the hcitools hcidump command which outputs all bluetooth activity."""
         self._log.info('Starting hcidump..')
         command = ['sudo', 'stdbuf', '-oL', 'hcidump']
         return subprocess.Popen(command, stdout=subprocess.PIPE)
 
     def log_beacons(self):
-        """
-        Overview:
-            Indefinitely logs the ID and RSSI of advertising Bluetooth LE devices.
-        """
-        # kickoff the lescan command
+        """Indefinitely logs the ID and RSSI of advertising Bluetooth LE devices."""
+        # Execute lescan command: listens for btle devices
         if not self._lescan():
             self._recover_lescan()
 
-        # get advertising devices
+        # Execute hcidump command: outputs all 
         hcidump = self._hcidump()
 
         # read device information
